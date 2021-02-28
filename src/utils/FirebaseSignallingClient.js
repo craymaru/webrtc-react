cecec2d8cb124760f0a8f9c8362fc1b682b4f033
Cray <craymaru@gmail.com>
2021-02-28 14:02:00 +0900

    feat: setup firebase

---
+++ src/utils/FirebaseSignallingClient.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app"
+import "firebase/database"
+
+export default class FirebaseSignallingClient {
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBbOU6-h7eFHB42_JdEzG1xaLKflkD_pog",
+      authDomain: "webrtc-react-cray.firebaseapp.com",
+      databaseURL: "https://webrtc-react-cray-default-rtdb.firebaseio.com",
+      projectId: "webrtc-react-cray",
+      storageBucket: "webrtc-react-cray.appspot.com",
+      messagingSenderId: "670746046536",
+      appId: "1:670746046536:web:33f9b5504e404e74ea287d",
+    }
+
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(firebaseConfig)
+    }
+    this.database = firebase.database()
+    this.localPeerName = ""
+    this.remotePeerName = ""
+  }
+}
